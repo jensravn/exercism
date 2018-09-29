b4b29c120c110a268900a06bf0710bebbd9df829
Jens Ravn Jensen <jr@jensravn.com>
2018-09-29 21:03:27 +0200

    Kindergarten garten solution 1

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -6,4 +6,36 @@ type Plant =
 | Radishes
 | Violets
 
-let plants diagram student = failwith "You need to implement this function."
+let plants (diagram: string) (student: string) =
+    let charToPlant char =
+        match char with
+        | 'R' -> Plant.Radishes
+        | 'C' -> Plant.Clover
+        | 'G' -> Plant.Grass
+        | 'V' -> Plant.Violets
+        | c -> failwith (sprintf "Unknown char: %A" c)
+
+    let studentToNumber student =
+        match student with
+        | "Alice" -> 0
+        | "Bob" -> 1
+        | "Charlie" -> 2
+        | "David" -> 3
+        | "Eve" -> 4
+        | "Fred"-> 5
+        | "Ginny" -> 6
+        | "Harriet" -> 7
+        | "Ileana" -> 8
+        | "Joseph" -> 9
+        | "Kincaid" -> 10
+        | "Larry" -> 11
+        | s -> failwith (sprintf "Unknown student: %A" s)
+
+    let getStudentsPlants (plantSequence: string) = 
+        let n = studentToNumber student
+        plantSequence.[n*2..n*2+1]
+
+    diagram.Split '\n'
+    |> Seq.collect getStudentsPlants
+    |> Seq.map charToPlant
+    |> Seq.toList
