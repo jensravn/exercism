8b4de1c79d2c9050938e3a186762c1407d9e0cd9
Jens Ravn Jensen <jr@jensravn.com>
2019-01-26 12:26:08 +0100

    Clock - first solution

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -1,9 +1,27 @@
 module Clock
 
-let create hours minutes = failwith "You need to implement this function."
+let minutesInDay clockInMinutes = clockInMinutes % (24 * 60)
 
-let add minutes clock = failwith "You need to implement this function."
+let positiveClockMinutes normalizedMinutes =
+        if normalizedMinutes < 0
+        then (24 * 60) + normalizedMinutes
+        else normalizedMinutes 
 
-let subtract minutes clock = failwith "You need to implement this function."
+let create hours minutes = 
+    hours * 60 + minutes
+    |> minutesInDay
+    |> positiveClockMinutes    
 
-let display clock = failwith "You need to implement this function."
+let add minutes clock = clock + minutes
+
+let subtract minutes clock = clock - minutes
+
+let display clockInMinutes = 
+    let positiveClockMinutes = 
+        clockInMinutes
+        |> minutesInDay
+        |> positiveClockMinutes    
+
+    let hours = positiveClockMinutes / 60
+    let minutes = positiveClockMinutes % 60
+    sprintf "%02i:%02i" hours minutes
