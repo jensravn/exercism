7438fd6951d3db2668fca90241a72daf41a4ed7b
Jens Ravn Jensen <jensravn102@pm.me>
2022-05-14 18:53:56 +0200

    bash two fer

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+
+echo "One for ${1:-you}, one for me."
